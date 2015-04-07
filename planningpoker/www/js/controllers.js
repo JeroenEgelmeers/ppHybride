3e4f770e3eca47ed63a05fa79568a66a4b2fd0f2
Metalmate <sprenkel_5@msn.com>
2015-04-07 17:39:37 +0200

    yourcard.html correct image + tap events to enlarge and un-enlarge the chosen card

--- planningpoker/www/js/controllers.js
+++ planningpoker/www/js/controllers.js
@@ -53,5 +53,28 @@ angular.module('starter.controllers', [])
   };
 })
 
+.controller('SelectedCardCntrl', function($scope, $ionicScrollDelegate)
+{
+  var imageIsFullscreen = false;
+
+  $scope.onImageTap = function ()
+  {
+  if (imageIsFullscreen)
+  {
+    $("#imageWrapper").removeAttr("style");
+    $("#your-card-image").removeAttr("style")
+  }
+  else
+  {
+    $ionicScrollDelegate.scrollTop()
+    $("#imageWrapper").parent().css({ "height":"calc(100% - 1px)" });
+    $("#imageWrapper").css({ "position":"absolute","left":"0","top":"0","max-width":"100%", "width":"100%", "height":"100%" });
+    $("#your-card-image").css({ "width": "100%", "height" : "100%" });
+  }
+
+  imageIsFullscreen = !imageIsFullscreen;
+  };
+})
+
 // .controller('PlaylistCtrl', function($scope, $stateParams) {
 // });
