2059a86f740865d7a54f655106d241365369a0b9
Jeroen Egelmeers <mail@jeroenegelmeers.nl>
2015-06-19 22:07:59 +0200

    nog een poging..

--- planningpoker/www/js/app.js
+++ planningpoker/www/js/app.js
@@ -4,6 +4,10 @@ angular.module('planningpoker', ['ionic', 'lbServices'])
 {
 	$ionicPlatform.ready(function ()
 	{
+		setTimeout(function() {
+	        navigator.splashscreen.hide();
+	    }, 10000);
+
 		if (window.cordova && window.cordova.plugins.Keyboard)
 		{
 			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
